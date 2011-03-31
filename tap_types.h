943089d4d6900c9216d3512b1677c3ab45aac4a0
fabbo <fabbo>
2011-03-31 18:23:02 +0000

    Put back blurb

--- tap_types.h
+++ tap_types.h
@@ -1,3 +1,22 @@
+/* TAP shared library: a library for converting audio data (as a stream of
+ * PCM signed 32-bit samples, mono) to raw TAP data and back
+ * 
+ * TAP specification was written by Per H�kan Sundell and is available at
+ * http://www.computerbrains.com/tapformat.html
+ * 
+ * The algorithm for TAP encoding was originally written by Janne Veli
+ * Kujala, based on the one written by Andreas Matthies for Tape64.
+ * Some modifications and adaptation to library format by Fabrizio Gennari
+ *
+ * The algorithm to decode TAP data into square, sine or triangular waves
+ * is by Fabrizio Gennari.
+ *
+ * Copyright (c) Fabrizio Gennari, 2003-2011
+ *
+ * The program is distributed under the GNU Lesser General Public License.
+ * See file LESSER-LICENSE.TXT for details.
+ */
+
 #ifndef TAP_TYPES_H
 #define TAP_TYPES_H
 
