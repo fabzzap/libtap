26c28b94a29135232ae08bea2f84dd64d464f67f
fabbo <fabbo>
2011-05-16 13:01:17 +0000

    Tweak to solve a tricky corner case with consecutive pulses of decreasing amplitude

--- tapencoder.c
+++ tapencoder.c
@@ -17,20 +17,26 @@
 #include <stdlib.h>
 #include "tapencoder.h"
 
+struct anomalies{
+  int32_t resolution_level;
+  uint32_t pos;
+  uint8_t rising;
+};
+
 struct tap_enc_t{
   uint32_t increasing;
   uint32_t prev_increasing;
   uint32_t input_pos;
   uint32_t min_duration;
   int32_t min_height;
-  int32_t trigger_level, anomalous_trigger_level;
+  int32_t trigger_level;
   int32_t val, prev_val, max_val, min_val;
-  uint32_t max, min, prev_max, prev_min, trigger_pos, anomalous_trigger_pos;
+  uint32_t max, min, prev_max, prev_min, trigger_pos;
   uint8_t sensitivity;
-  uint8_t anomalous_trigger;
   enum tap_trigger trigger_type;
   uint8_t initial_threshold;
-  uint8_t triggered;
+  uint8_t triggered, cached_trigger;
+  struct anomalies *anomaly, *old_anomaly;
 };
 
 static void reset_state(struct tap_enc_t *tap){
@@ -46,10 +52,12 @@ static void reset_state(struct tap_enc_t *tap){
   tap->max_val= (tap->initial_threshold<<24);
   tap->min_val=-(tap->initial_threshold<<24);
   tap->triggered = 1;
-  tap->anomalous_trigger_level = 0;
+  tap->cached_trigger = 0;
   tap->trigger_level = 0;
-  tap->anomalous_trigger = 0;
-  tap->anomalous_trigger_pos = 0;
+  free(tap->anomaly);
+  free(tap->old_anomaly);
+  tap->anomaly = NULL;
+  tap->old_anomaly = NULL;
 }
 
 static uint32_t set_trigger(uint32_t trigger_pos, uint32_t *stored_trigger_pos, uint8_t *got_pulse) {
@@ -70,8 +78,22 @@ struct tap_enc_t *tapenc_init(uint32_t min_duration, uint8_t sensitivity, uint8_
   tap->initial_threshold = initial_threshold > 127 ? 127 : initial_threshold;
   tap->trigger_type=inverted;
   tap->sensitivity=sensitivity > 100 ? 100 : sensitivity;
+  tap->anomaly = NULL;
+  tap->old_anomaly = NULL;
   reset_state(tap);
   return tap;
+}
+
+static void set_anomaly(struct tap_enc_t *tap, uint32_t prev_minmax, uint8_t rising){
+  struct anomalies *anomaly = malloc(sizeof(struct anomalies));
+
+  anomaly->resolution_level = tap->trigger_level;
+  anomaly->pos = tap->input_pos / 2 + prev_minmax / 2;
+  anomaly->rising = rising;
+
+  if(tap->anomaly != NULL)
+    tap->old_anomaly = tap->anomaly;
+  tap->anomaly = anomaly;
 }
 
 uint32_t tapenc_get_pulse(struct tap_enc_t *tap, int32_t *buffer, unsigned int buflen, uint8_t *got_pulse, uint32_t *pulse){
@@ -79,12 +101,20 @@ uint32_t tapenc_get_pulse(struct tap_enc_t *tap, int32_t *buffer, unsigned int b
 
   *got_pulse = 0;
 
-  while(!(*got_pulse) && samples_done < buflen){
-    if(tap->anomalous_trigger){
-      tap->anomalous_trigger = 0;
-      *pulse = set_trigger(tap->input_pos - 1, &tap->trigger_pos, got_pulse);
-      break;
+  while(!(*got_pulse)){
+    if(tap->cached_trigger){
+      tap->cached_trigger = 0;
+      if(
+         (tap->min > tap->max && tap->trigger_type != TAP_TRIGGER_ON_FALLING_EDGE)
+      || (tap->min < tap->max && tap->trigger_type != TAP_TRIGGER_ON_RISING_EDGE)
+        ){
+        *pulse = set_trigger(tap->input_pos - 1, &tap->trigger_pos, got_pulse);
+        break;
+      }
     }
+    if(samples_done >= buflen)
+      break;
+
     tap->prev_val = tap->val;
     tap->val = buffer[samples_done++];
 
@@ -94,23 +124,22 @@ uint32_t tapenc_get_pulse(struct tap_enc_t *tap, int32_t *buffer, unsigned int b
     else if (tap->val < tap->prev_val)
       tap->increasing = 0;
 
-    if (tap->increasing != tap->prev_increasing && tap->anomalous_trigger_pos == 0) /* A min or max has been reached. Is it a true one? */
+    if (tap->increasing != tap->prev_increasing) /* A min or max has been reached. Is it a true one? */
     {
       if (tap->increasing
        && tap->input_pos - tap->max > tap->min_duration
        && tap->min_height > tap->prev_val
        && (tap->max>tap->min || tap->min_val > tap->prev_val) ){ /* A minimum */
         if (tap->max >= tap->min) {
-          if (tap->triggered == 0) {
-            tap->anomalous_trigger_pos = tap->input_pos / 2 + tap->max / 2;
-            tap->anomalous_trigger_level = tap->trigger_level;
-          }
-          tap->triggered = 0;
+          if (tap->triggered == 0) 
+            set_anomaly(tap, tap->max, 0);
+          else
+            tap->triggered = 0;
+          tap->prev_min = tap->min;
         }
         tap->trigger_level = tap->max > 0 ?
           tap->prev_val / 2 + tap->max_val / 2
           : 0;
-        tap->prev_min = tap->min;
         tap->min = tap->input_pos;
         tap->min_val = tap->prev_val;
         tap->min_height = tap->max>0 ?
@@ -122,16 +151,15 @@ uint32_t tapenc_get_pulse(struct tap_enc_t *tap, int32_t *buffer, unsigned int b
              && tap->prev_val > tap->min_height
              && (tap->min>tap->max || tap->prev_val > tap->max_val) ){ /* A maximum */
         if (tap->min >= tap->max) {
-          if (tap->triggered == 0) {
-            tap->anomalous_trigger_pos = tap->input_pos / 2 + tap->max / 2;
-            tap->anomalous_trigger_level = tap->trigger_level;
-          }
-          tap->triggered = 0;
+          if (tap->triggered == 0)
+            set_anomaly(tap, tap->min, 1);
+          else
+            tap->triggered = 0;
+          tap->prev_max = tap->max;
         }
         tap->trigger_level =  tap->min > 0 ?
           tap->prev_val / 2 + tap->min_val / 2
           : 0;
-        tap->prev_max = tap->max;
         tap->max = tap->input_pos;
         tap->max_val = tap->prev_val;
         tap->min_height = tap->min>0 ?
@@ -139,42 +167,47 @@ uint32_t tapenc_get_pulse(struct tap_enc_t *tap, int32_t *buffer, unsigned int b
             : -tap->initial_threshold<<24;
       }
     }
-    if(!tap->triggered) {
-      if (tap->anomalous_trigger_pos > 0
-       && tap->min > tap->max
-       && tap->val < tap->anomalous_trigger_level){
-        tap->anomalous_trigger_pos = 0;
+    if(!tap->triggered && tap->anomaly != NULL){
+      if(tap->min > tap->max
+      && tap->val < tap->anomaly->resolution_level){
+        free(tap->anomaly);
+        tap->anomaly = NULL;
         tap->triggered = 1;
+        tap->cached_trigger = 1;
         tap->min = tap->prev_min;
       }
-      else if (tap->anomalous_trigger_pos > 0
-               && tap->min < tap->max
-               && tap->val > tap->anomalous_trigger_level){
-        tap->anomalous_trigger_pos = 0;
+      else if (tap->min < tap->max
+            && tap->val > tap->anomaly->resolution_level){
+        free(tap->anomaly);
+        tap->anomaly = NULL;
         tap->triggered = 1;
+        tap->cached_trigger = 1;
         tap->max = tap->prev_max;
       }
-      else if ((tap->min > tap->max && tap->val > tap->trigger_level)
-            || (tap->min < tap->max && tap->val < tap->trigger_level)){
-        tap->triggered = 1;
-        tap->anomalous_trigger = tap->anomalous_trigger_pos > 0;
+    }
+    if(!tap->triggered && 
+       (
+        (tap->min > tap->max && tap->val > tap->trigger_level)
+     || (tap->min < tap->max && tap->val < tap->trigger_level)
+       )
+      ){
+      tap->triggered = 1;
+      tap->cached_trigger = 1;
+    }
+    if (tap->anomaly &&
+        (tap->cached_trigger || tap->old_anomaly)
+       ){
+      if (!tap->old_anomaly){
+        tap->old_anomaly = tap->anomaly;
+        tap->anomaly = NULL;
       }
-      if (tap->triggered){
-        uint8_t trigger_causes_pulse =
-          (tap->val > tap->trigger_level && tap->trigger_type != TAP_TRIGGER_ON_FALLING_EDGE)
-       || (tap->val < tap->trigger_level && tap->trigger_type != TAP_TRIGGER_ON_RISING_EDGE);
-        if (trigger_causes_pulse && !tap->anomalous_trigger)
-          *pulse = set_trigger(tap->input_pos, &tap->trigger_pos, got_pulse);
-        else if(tap->anomalous_trigger &&
-                (
-                 (tap->val > tap->trigger_level && tap->trigger_type != TAP_TRIGGER_ON_RISING_EDGE)
-              || (tap->val < tap->trigger_level && tap->trigger_type != TAP_TRIGGER_ON_FALLING_EDGE)
-                )
-               )
-          *pulse = set_trigger(tap->anomalous_trigger_pos, &tap->trigger_pos, got_pulse);
-        tap->anomalous_trigger_pos = 0;
-        tap->anomalous_trigger = tap->anomalous_trigger && trigger_causes_pulse;
-      }
+      if(
+         (!tap->old_anomaly->rising && tap->trigger_type != TAP_TRIGGER_ON_RISING_EDGE)
+      || ( tap->old_anomaly->rising && tap->trigger_type != TAP_TRIGGER_ON_FALLING_EDGE)
+         )
+        *pulse = set_trigger(tap->old_anomaly->pos, &tap->trigger_pos, got_pulse);
+      free(tap->old_anomaly);
+      tap->old_anomaly = NULL;
     }
     tap->input_pos++;
   }
